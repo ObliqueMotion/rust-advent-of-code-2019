c3e2b369929fd0c9ae040daddb7158df82f19414
Erik Nordin <aeketn@gmail.com>
2019-12-02 23:08:25 -0800

    AoC 2019 Day03 Part2

--- src/day03.rs
+++ src/day03.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::ops::AddAssign;
 use Direction::*;
 
 static INPUT: &str = include_str!("../inputs/day03");
@@ -10,9 +11,10 @@ enum Direction {
     Left,
 }
 
-impl Direction {
-    fn update(&self, x: &mut i64, y: &mut i64) {
-        match self {
+impl AddAssign<&Direction> for (i32, i32) {
+    fn add_assign(&mut self, other: &Direction) {
+        let (x, y) = self;
+        match other {
             Up    => *y += 1,
             Right => *x += 1,
             Down  => *y -= 1,
@@ -21,8 +23,8 @@ impl Direction {
     }
 }
 
-fn parse_instruction(instruction: &str) -> (Direction, i64) {
-    match (instruction.chars().next(), instruction[1..].parse::<i64>()) {
+fn parse_instruction(instruction: &str) -> (Direction, i32) {
+    match (instruction.chars().next(), instruction[1..].parse::<i32>()) {
         (Some(direction), Ok(distance)) => match direction {
             'U' => (Up,    distance),
             'R' => (Right, distance),
@@ -34,26 +36,21 @@ fn parse_instruction(instruction: &str) -> (Direction, i64) {
     }
 }
 
-fn map_coords_to_steps(input: &str) -> HashMap<(i64, i64), usize> {
-    let mut x = 0;
-    let mut y = 0;
+fn map_coords_to_steps(input: &str) -> HashMap<(i32, i32), usize> {
     let mut step = 0;
-    let mut coordinates = HashMap::new();
+    let mut coord = (0, 0);
+    let mut coord_map = HashMap::new();
     let instructions = input.trim().split(',').map(parse_instruction);
     
-    let mut add_entries_for = |direction: &Direction| {
-        step += 1;
-        direction.update(&mut x, &mut y);
-        coordinates.entry((x, y)).or_insert(step);
-    };
-
     for (direction, distance) in instructions {
         for _ in 0..distance {
-            add_entries_for(&direction)
+            step += 1;
+            coord += &direction;
+            coord_map.entry(coord).or_insert(step);
         }
     }
     
-    coordinates
+    coord_map
 }
 
 pub fn part1() {
@@ -61,10 +58,10 @@ pub fn part1() {
     let input1 = lines.next().expect("Failed to read puzzle input.");
     let input2 = lines.next().expect("Failed to read puzzle input.");
 
-    let cts1 = map_coords_to_steps(input1);
-    let cts2 = map_coords_to_steps(input2);
+    let wire1 = map_coords_to_steps(input1);
+    let wire2 = map_coords_to_steps(input2);
 
-    let min_distance = cts1.keys().filter(|k| cts2.contains_key(k))
+    let min_distance = wire1.keys().filter(|k| wire2.contains_key(k))
         .map(|(x, y)| x.abs() + y.abs())
         .min();
 
@@ -76,11 +73,11 @@ pub fn part2() {
     let input1 = lines.next().expect("Failed to read puzzle input.");
     let input2 = lines.next().expect("Failed to read puzzle input.");
 
-    let cts1 = map_coords_to_steps(input1);
-    let cts2 = map_coords_to_steps(input2);
+    let wire1 = map_coords_to_steps(input1);
+    let wire2 = map_coords_to_steps(input2);
 
-    let min_steps = cts1.keys().filter(|k| cts2.contains_key(k))
-        .map(|k| cts1[k] + cts2[k])
+    let min_steps = wire1.keys().filter(|k| wire2.contains_key(k))
+        .map(|k| wire1[k] + wire2[k])
         .min();
 
     println!("Day03 Part2: {:?}", min_steps);
